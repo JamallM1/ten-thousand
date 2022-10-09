f043e2c2b2ab02502df6714b1a064fa9bc070a03
Jamall Malik <mimsjamall79@gmail.com>
2022-10-09 12:47:27 -0400

    finished lab test pass

---
+++ ten_thousand/score.py
@@ -0,0 +1,21 @@
+class Banker:
+    def __init__(self):
+        self.score = 0
+        self.round_score = 0
+
+    def add_score(self, points):
+        self.round_score += points
+
+    def get_round_score(self):
+        return self.round_score
+
+    def abort_round_score(self):
+        self.round_score = 0
+
+    def commit_round_score(self):
+        self.score += self.round_score
+        self.round_score = 0
+
+    def get_score(self):
+        return self.score
+    
